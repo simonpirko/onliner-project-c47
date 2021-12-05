a92e75b51e33fd26f8d286cd3b7c1cc5df1c368a
Dimon <dima2000@tut.by>
2021-12-05 22:51:26 +0300

    add authorization shop servlet, method, jsp

--- src/main/java/by/fakeonliner/service/ShopService.java
+++ src/main/java/by/fakeonliner/service/ShopService.java
@@ -1,4 +1,21 @@
 package by.fakeonliner.service;
 
+import by.fakeonliner.entity.Shop;
+import by.fakeonliner.repository.ShopRepository;
+
+import java.util.List;
+
 public class ShopService {
+    private ShopRepository shopRepository = new ShopRepository();
+
+    public Shop findByLogin(String login) {
+        if (shopRepository.existByLogin(login)) {
+            return shopRepository.getShop(login);
+        }
+        return null;
+    }
+
+    public List getShopList() {
+        return shopRepository.getShops();
+    }
 }
