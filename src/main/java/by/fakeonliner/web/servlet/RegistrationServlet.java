9c4b7094189a41e377ebf147f406d2ab0652e18a
TorusTredent <torus.tredent@gmail.com>
2021-12-06 22:32:26 +0300

    add private methode isCorrectEmail and phone

--- src/main/java/by/fakeonliner/web/servlet/RegistrationServlet.java
+++ src/main/java/by/fakeonliner/web/servlet/RegistrationServlet.java
@@ -27,74 +27,63 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String phoneNumber = req.getParameter("phoneNumber");
         User user = new User();
         boolean flagMessage = false;
 
-        if (regValid.isNullOrEmpty(req.getParameter("firstName"))) {
+        if (regValid.isNullOrEmpty(firstName)) {
             req.getSession().setAttribute("firstNameMessage", ConstantMessage.FIRST_NAME_IS_EMPTY.toString());
             flagMessage = true;
         } else {
-            user.setFirstName(req.getParameter("firstName"));
+            user.setFirstName(firstName);
         }
 
-        if (regValid.isNullOrEmpty(req.getParameter("lastName"))) {
+        if (regValid.isNullOrEmpty(lastName)) {
             req.getSession().setAttribute("lastNameMessage", ConstantMessage.LAST_NAME_IS_EMPTY.toString());
             flagMessage = true;
         } else {
-            user.setLastName(req.getParameter("lastName"));
+            user.setLastName(lastName);
         }
 
-        if (regValid.isNullOrEmpty(req.getParameter("username"))) {
+        if (regValid.isNullOrEmpty(username)) {
             req.getSession().setAttribute("usernameMessage", ConstantMessage.USERNAME_IS_EMPTY.toString());
             flagMessage = true;
         } else {
-            user.setUsername(req.getParameter("username"));
+            user.setUsername(username);
         }
 
-        if (regValid.isNullOrEmpty(req.getParameter("password"))) {
+        if (regValid.isNullOrEmpty(password)) {
             req.getSession().setAttribute("passwordMessage", ConstantMessage.PASSWORD_IS_EMPTY.toString());
             flagMessage = true;
         } else {
-            user.setPassword(req.getParameter("password"));
+            user.setPassword(password);
         }
 
-        if (regValid.isNullOrEmpty(req.getParameter("confirmPassword"))) {
-            req.getSession().setAttribute("confirmPasswordMessage", ConstantMessage.PASSWORD_IS_EMPTY.toString());
-            flagMessage = true;
-        }
-
-        if (regValid.isNullOrEmpty(req.getParameter("email"))) {
+        if (regValid.isNullOrEmpty(email)) {
             req.getSession().setAttribute("emailMessage", ConstantMessage.EMAIL_IS_EMPTY.toString());
             flagMessage = true;
         } else {
-            user.setEmail(req.getParameter("email"));
+            user.setEmail(email);
         }
 
-        if (regValid.isNullOrEmpty(req.getParameter("phoneNumber"))) {
+        if (regValid.isNullOrEmpty(phoneNumber)) {
             req.getSession().setAttribute("phoneNumberMessage", ConstantMessage.PHONE_NUMBER_IS_EMPTY.toString());
             flagMessage = true;
         } else {
-            user.setPhoneNumber(req.getParameter("phoneNumber"));
+            user.setPhoneNumber(phoneNumber);
         }
 
-        if (regValid.isCorrectEmail(req.getParameter("email"))) {
-            if (userService.existByEmail(req.getParameter("email"))) {
-                req.getSession().setAttribute("email", ConstantMessage.EMAIL_ALREADY_EXIST.toString());
-                flagMessage = true;
-            }
-        } else {
-            req.getSession().setAttribute("email", ConstantMessage.EMAIL_INPUT_INCORRECTLY.toString());
-            flagMessage = true;
+        if (!flagMessage) {
+            flagMessage = isCorrectEmail(email, req);
         }
 
-        if (regValid.isCorrectPhone(req.getParameter("phoneNumber"))) {
-            if (userService.existByPhoneNumber(req.getParameter("phoneNumber"))) {
-                req.getSession().setAttribute("phoneNumberMessage", ConstantMessage.PHONE_NUMBER_ALREADY_EXIST.toString());
-                flagMessage = true;
-            }
-        } else {
-            req.getSession().setAttribute("phoneNumberMessage", ConstantMessage.PHONE_NUMBER_INPUT_INCORRECTLY.toString());
-            flagMessage = true;
+        if (!flagMessage) {
+            flagMessage = isCorrectPhoneNumber(phoneNumber, req);
         }
 
         if (userService.existByUsername(user.getUsername())) {
@@ -114,5 +103,32 @@ public class RegistrationServlet extends HttpServlet {
     public void init() throws ServletException {
         userService = new UserService();
         regValid = new RegistrationValidator();
+    }
+
+
+    private boolean isCorrectPhoneNumber(String phoneNumber, HttpServletRequest req) {
+        if (regValid.isCorrectPhone(phoneNumber)) {
+            if (userService.existByPhoneNumber(phoneNumber)) {
+                req.getSession().setAttribute("phoneNumberMessage", ConstantMessage.PHONE_NUMBER_ALREADY_EXIST.toString());
+                return true;
+            }
+        } else {
+            req.getSession().setAttribute("phoneNumberMessage", ConstantMessage.PHONE_NUMBER_INPUT_INCORRECTLY.toString());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isCorrectEmail(String email, HttpServletRequest req) {
+        if (regValid.isCorrectEmail(email)) {
+            if (userService.existByEmail(email)) {
+                req.getSession().setAttribute("email", ConstantMessage.EMAIL_ALREADY_EXIST.toString());
+                return true;
+            }
+        } else {
+            req.getSession().setAttribute("email", ConstantMessage.EMAIL_INPUT_INCORRECTLY.toString());
+            return true;
+        }
+        return false;
     }
 }
