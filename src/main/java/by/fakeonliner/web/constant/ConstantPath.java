695ba73636bbdbda955efe76b24babdf96c830ce
TorusTredent <torus.tredent@gmail.com>
2021-12-08 11:46:25 +0300

    remove conflict and manage folders

--- src/main/java/by/fakeonliner/web/constant/ConstantPath.java
+++ src/main/java/by/fakeonliner/web/constant/ConstantPath.java
@@ -1,6 +1,7 @@
 package by.fakeonliner.web.constant;
 
 public final class ConstantPath {
-    public final static String USER_REGISTRATION_JSP = "/pages/reg.jsp";
+    public final static String USER_REGISTRATION_JSP = "/pages/user/reg.jsp";
     public final static String SHOP_AUTHORIZATION_JSP = "/pages/shop/authorization.jsp";
+    public final static String USER_AUTHORIZATION_JSP = "/pages/user/authorization.jsp";
 }
