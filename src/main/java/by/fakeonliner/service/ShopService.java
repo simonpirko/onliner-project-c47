49fe5677b4dbf8d465df6ba92f85d61edd7e0f62
dima2000@tut.by <Z18101976Dd>
2021-12-08 09:31:36 +0300

    fix

--- src/main/java/by/fakeonliner/service/ShopService.java
+++ src/main/java/by/fakeonliner/service/ShopService.java
@@ -5,11 +5,11 @@ import by.fakeonliner.repository.ShopDao;
 import by.fakeonliner.repository.ShopRepository;
 
 public class ShopService {
-    private final ShopDao shopRepository = new ShopRepository();
+    private final ShopDao shopDao = new ShopRepository();
 
     public boolean registerShop(Shop shop) {
-        if (!shopRepository.existByLogin(shop.getLogin())) {
-            shopRepository.save(shop);
+        if (!shopDao.existByLogin(shop.getLogin())) {
+            shopDao.save(shop);
             return true;
         }
         return false;
