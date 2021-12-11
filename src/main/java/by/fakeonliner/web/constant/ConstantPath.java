68af3e156d2ef177c29ab1e6856e788e49c4a4b6
Dimon <dima2000@tut.by>
2021-12-11 20:34:02 +0300

    add constant

--- src/main/java/by/fakeonliner/web/constant/ConstantPath.java
+++ src/main/java/by/fakeonliner/web/constant/ConstantPath.java
@@ -2,6 +2,12 @@ package by.fakeonliner.web.constant;
 
 public final class ConstantPath {
 
+    public final static String HOME_JSP = "/pages/home.jsp";
+    public final static String USER_PROFILE_JSP = "/pages/user/user_profile.jsp";
+    public final static String ADMIN_USERS_JSP = "/pages/admin/users.jsp";
+    public final static String ADMIN_PRODUCTS_JSP = "/pages/admin/products.jsp";
+    public final static String ADMIN_CATEGORIES_JSP = "/pages/admin/categories.jsp";
+    public final static String CHANGE_DATA_USER_JSP ="/pages/user/change_data_user.jsp";
     public final static String COMMENT_ADD_JSP = "/pages/comment/commentAdd.jsp";
     public final static String USER_REGISTRATION_JSP = "/pages/user/reg.jsp";
     public final static String USER_AUTHORIZATION_JSP = "/pages/user/authorization.jsp";
