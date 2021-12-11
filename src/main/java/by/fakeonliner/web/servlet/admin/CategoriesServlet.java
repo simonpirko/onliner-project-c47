3335aa487f7c3d0354ab76f155e3ff4765f5b708
TorusTredent <torus.tredent@gmail.com>
2021-12-11 15:51:33 +0300

    categories

--- src/main/java/by/fakeonliner/web/servlet/admin/CategoriesServlet.java
+++ src/main/java/by/fakeonliner/web/servlet/admin/CategoriesServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(value = "/admin/users", name = "CategoriesServlet")
+@WebServlet(value = "/admin/categories", name = "CategoriesServlet")
 public class CategoriesServlet extends HttpServlet {
 
     @Override
